641b1bf99abcde28fcbbdc287019e9c6e3d044a0
Sepp Wikramaratna-Clarke <45594209+Phl3bas@users.noreply.github.com>
2019-07-19 16:09:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,65 @@
-const menu = document.querySelector('.menu');
-const topBar = document.querySelector('.menu__bar--top');
-const midBar = document.querySelector('.menu__bar--middle');
-const botBar = document.querySelector('.menu__bar--bottom');
+const menu1 = document.querySelector('#menu1');
+const topBar1 = document.querySelector('#menu1 .menu__bar--top');
+const midBar1 = document.querySelector('#menu1 .menu__bar--middle');
+const botBar1 = document.querySelector('#menu1 .menu__bar--bottom');
 
-menu.addEventListener('click',()=>{
-	topBar.classList.toggle('menu__bar--rotateLeft');
-	midBar.classList.toggle('menu__bar--hide');
-	botBar.classList.toggle('menu__bar--rotateRight');
+menu1.addEventListener('click',()=>{
+	topBar1.classList.toggle('menu__bar--rotateLeft');
+	midBar1.classList.toggle('menu__bar--hide');
+	botBar1.classList.toggle('menu__bar--rotateRight');
+});
+
+const menu2 = document.querySelector('#menu2');
+const topBar2 = document.querySelector('#menu2 .menu__bar--top');
+const midBar2 = document.querySelector('#menu2 .menu__bar--middle');
+const botBar2 = document.querySelector('#menu2 .menu__bar--bottom');
+
+menu2.addEventListener('click',()=>{
+	topBar2.classList.toggle('menu__bar--rotateLeft');
+	midBar2.classList.toggle('menu__bar--hide');
+	botBar2.classList.toggle('menu__bar--rotateRight');
+});
+
+const menu3 = document.querySelector('#menu3');
+const topBar3 = document.querySelector('#menu3 .menu__bar--top');
+const midBar3 = document.querySelector('#menu3 .menu__bar--middle');
+const botBar3 = document.querySelector('#menu3 .menu__bar--bottom');
+
+menu3.addEventListener('click',()=>{
+	topBar3.classList.toggle('menu__bar--rotateLeft');
+	midBar3.classList.toggle('menu__bar--hide');
+	botBar3.classList.toggle('menu__bar--rotateRight');
+});
+
+const menu4 = document.querySelector('#menu4');
+const topBar4 = document.querySelector('#menu4 .menu__bar--top');
+const midBar4 = document.querySelector('#menu4 .menu__bar--middle');
+const botBar4 = document.querySelector('#menu4 .menu__bar--bottom');
+
+menu4.addEventListener('click',()=>{
+	topBar4.classList.toggle('menu__bar--rotateLeft');
+	midBar4.classList.toggle('menu__bar--hide');
+	botBar4.classList.toggle('menu__bar--rotateRight');
+});
+
+const menu5 = document.querySelector('#menu5');
+const topBar5 = document.querySelector('#menu5 .menu__bar--top');
+const midBar5 = document.querySelector('#menu5 .menu__bar--middle');
+const botBar5 = document.querySelector('#menu5 .menu__bar--bottom');
+
+menu5.addEventListener('click',()=>{
+	topBar5.classList.toggle('menu__bar--rotateLeft');
+	midBar5.classList.toggle('menu__bar--hide');
+	botBar5.classList.toggle('menu__bar--rotateRight');
+});
+
+const menu6 = document.querySelector('#menu6');
+const topBar6 = document.querySelector('#menu6 .menu__bar--top');
+const midBar6 = document.querySelector('#menu6 .menu__bar--middle');
+const botBar6 = document.querySelector('#menu6 .menu__bar--bottom');
+
+menu6.addEventListener('click',()=>{
+	topBar6.classList.toggle('menu__bar--rotateLeft');
+	midBar6.classList.toggle('menu__bar--hide');
+	botBar6.classList.toggle('menu__bar--rotateRight');
 });
