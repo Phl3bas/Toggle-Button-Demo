b6381167a9f591b3e0aca949b16be0b7abc42109
Sepp Wikramaratna-Clarke <45594209+Phl3bas@users.noreply.github.com>
2019-07-22 17:24:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,51 +15,43 @@ const midBar2 = document.querySelector('#menu2 .menu__bar--middle');
 const botBar2 = document.querySelector('#menu2 .menu__bar--bottom');
 
 menu2.addEventListener('click',()=>{
-	topBar2.classList.toggle('menu__bar--rotateLeft');
-	midBar2.classList.toggle('menu__bar--hide');
-	botBar2.classList.toggle('menu__bar--rotateRight');
+	topBar2.classList.toggle('menu__transition2--top');
+	midBar2.classList.toggle('menu__transition2--middle');
+	botBar2.classList.toggle('menu__transition2--bottom');
 });
 
 const menu3 = document.querySelector('#menu3');
 const topBar3 = document.querySelector('#menu3 .menu__bar--top');
 const midBar3 = document.querySelector('#menu3 .menu__bar--middle');
 const botBar3 = document.querySelector('#menu3 .menu__bar--bottom');
+let state = false;
 
 menu3.addEventListener('click',()=>{
-	topBar3.classList.toggle('menu__bar--rotateLeft');
-	midBar3.classList.toggle('menu__bar--hide');
-	botBar3.classList.toggle('menu__bar--rotateRight');
-});
+	
 
-const menu4 = document.querySelector('#menu4');
-const topBar4 = document.querySelector('#menu4 .menu__bar--top');
-const midBar4 = document.querySelector('#menu4 .menu__bar--middle');
-const botBar4 = document.querySelector('#menu4 .menu__bar--bottom');
-
-menu4.addEventListener('click',()=>{
-	topBar4.classList.toggle('menu__bar--rotateLeft');
-	midBar4.classList.toggle('menu__bar--hide');
-	botBar4.classList.toggle('menu__bar--rotateRight');
-});
-
-const menu5 = document.querySelector('#menu5');
-const topBar5 = document.querySelector('#menu5 .menu__bar--top');
-const midBar5 = document.querySelector('#menu5 .menu__bar--middle');
-const botBar5 = document.querySelector('#menu5 .menu__bar--bottom');
-
-menu5.addEventListener('click',()=>{
-	topBar5.classList.toggle('menu__bar--rotateLeft');
-	midBar5.classList.toggle('menu__bar--hide');
-	botBar5.classList.toggle('menu__bar--rotateRight');
-});
-
-const menu6 = document.querySelector('#menu6');
-const topBar6 = document.querySelector('#menu6 .menu__bar--top');
-const midBar6 = document.querySelector('#menu6 .menu__bar--middle');
-const botBar6 = document.querySelector('#menu6 .menu__bar--bottom');
-
-menu6.addEventListener('click',()=>{
-	topBar6.classList.toggle('menu__bar--rotateLeft');
-	midBar6.classList.toggle('menu__bar--hide');
-	botBar6.classList.toggle('menu__bar--rotateRight');
+	if (!state){
+		topBar3.classList.add('menu__transition3-top-start');
+		midBar3.classList.add('menu__transition3-middle');
+		botBar3.classList.add('menu__transition3-bottom-start');
+		
+		setTimeout(function(){
+			topBar3.classList.add('menu__transition3-top-end');
+			botBar3.classList.add('menu__transition3-bottom-end');
+		},300);
+		
+		state = true;
+		
+	} else {
+		
+		topBar3.classList.remove('menu__transition3-top-end');
+		botBar3.classList.remove('menu__transition3-bottom-end');
+		
+		setTimeout(function(){
+			topBar3.classList.remove('menu__transition3-top-start');
+			midBar3.classList.remove('menu__transition3-middle');
+			botBar3.classList.remove('menu__transition3-bottom-start');
+			},300);
+			
+		state = false;
+	}
 });
